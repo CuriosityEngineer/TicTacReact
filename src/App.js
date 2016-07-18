8b9e580d4b81ac1f3d26c5f0a82ad5ec643f0c3b
CuriosityEngineer <aeshta@gmail.com>
2016-07-18 21:34:21 +0200

    add models

--- src/App.js
+++ src/App.js
@@ -1,6 +1,56 @@
 import React from 'react';
+import GameModel from '../models/GameModel';
 
 class App extends React.Component {
+  constructor(){
+    super();
+
+    this.games = new GameModel();
+    this.games.subscribe(this.updateList.bind(this));
+  }
+
+  updateList() {
+    console.log(this.games.resources);
+
+    this.setState({
+      games: this.games.resources
+    });
+  }
+
+  createGame() {
+    this.games.addResource({
+      playerOne: this.state.currentPlayer
+    });
+  }
+
+  joinGame(game) {
+    console.log("Joining game...");
+    if (gameIsAvailable(game)) {
+      if (gameIsJoinable(game)) {
+        console.log("Joining game as player two...");
+        this.games.save(game, { playerTwo: this.state.currentPlayer });
+      }
+
+      this.setState({
+        currentGame: game
+      });
+    } else {
+      window.alert("Can't touch this dung dung dung dung");
+    }
+  }
+
+  gameIsAvailable(game) {
+    return (game.playerOne === this.state.currentPlayer ||
+      game.playerTwo === this.state.currentPlayer ||
+      game.playerTwo === null);
+  }
+
+  gameIsJoinable(game) {
+    return  (game.playerOne !== this.state.currentPlayer && game.playerTwo !== this.state.currentPlayer);
+  }
+
+  this.games.save(game, { playerTwo: this.state.currentPlayer });
+
     render() {
         return (
             <h1>Hello World!</h1>
