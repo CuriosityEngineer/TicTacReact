f92aef803b939702655986563304cb96d27b77ef
CuriosityEngineer <aeshta@gmail.com>
2016-07-18 21:52:16 +0200

    install feathers and fix basemodel

--- src/App.js
+++ src/App.js
@@ -1,61 +1,14 @@
 import React from 'react';
-import GameModel from '../models/GameModel';
+import GameModel from './models/GameModel';
 
 class App extends React.Component {
-  constructor(){
-    super();
-
-    this.games = new GameModel();
-    this.games.subscribe(this.updateList.bind(this));
+  render() {
+    return (
+      <div>
+        <h1>Tic Tac Toe!</h1>
+      </div>
+    );
   }
-
-  updateList() {
-    console.log(this.games.resources);
-
-    this.setState({
-      games: this.games.resources
-    });
-  }
-
-  createGame() {
-    this.games.addResource({
-      playerOne: this.state.currentPlayer
-    });
-  }
-
-  joinGame(game) {
-    console.log("Joining game...");
-    if (gameIsAvailable(game)) {
-      if (gameIsJoinable(game)) {
-        console.log("Joining game as player two...");
-        this.games.save(game, { playerTwo: this.state.currentPlayer });
-      }
-
-      this.setState({
-        currentGame: game
-      });
-    } else {
-      window.alert("Can't touch this dung dung dung dung");
-    }
-  }
-
-  gameIsAvailable(game) {
-    return (game.playerOne === this.state.currentPlayer ||
-      game.playerTwo === this.state.currentPlayer ||
-      game.playerTwo === null);
-  }
-
-  gameIsJoinable(game) {
-    return  (game.playerOne !== this.state.currentPlayer && game.playerTwo !== this.state.currentPlayer);
-  }
-
-  this.games.save(game, { playerTwo: this.state.currentPlayer });
-
-    render() {
-        return (
-            <h1>Hello World!</h1>
-        );
-    }
 }
 
 export default App;
