8d0232cb3c9814fee12f05b8688121084612225f
CuriosityEngineer <aeshta@gmail.com>
2016-07-19 12:56:42 +0200

    fix show grid

--- src/containers/board.js
+++ src/containers/board.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Grid from '../components/grid';
+import Grid2 from '../components/grid2';
 
 class Board extends React.Component {
   render() {
     return (
       <div>
         <h1>Tic Tac Toe! Board</h1>
-        <Grid/>
+        <Grid2/>
       </div>
     );
   }
